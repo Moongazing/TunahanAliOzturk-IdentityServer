799551f21f2a04b5eb2f24d4d50665ba2fa0a522
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-06 21:08:52 +0300

    Adding Admin Area

--- ValidationRules/FluentValidation/SignUpViewModelValidator.cs
+++ ValidationRules/FluentValidation/SignUpViewModelValidator.cs
@@ -12,14 +12,14 @@ namespace TAO.IdentityApp.Web.ValidationRules.FluentValidation
             RuleFor(x => x.UserName).NotEmpty().WithMessage($"Please enter a valid user name.");
             RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Please enter a valid email address.");
             RuleFor(x => x.Phone).NotEmpty().WithMessage("Please enter a valid phone number \n Example: 555555555");
-            RuleFor(x => x.Password).NotEmpty().Must(IsPasswordValid).Matches(x=>x.PasswordConfirm).WithMessage("Password must be match.");
-            RuleFor(x => x.PasswordConfirm).NotEmpty().Must(IsPasswordValid).Matches(x=>x.Password).WithMessage("Password must be match.");
+            RuleFor(x => x.Password).NotEmpty().Matches(x=>x.PasswordConfirm).WithMessage("Password must be match.");
+            RuleFor(x => x.PasswordConfirm).NotEmpty().Matches(x=>x.Password).WithMessage("Password must be match.");
         }
        
-        private bool IsPasswordValid(string arg)
+       /* private bool IsPasswordValid(string arg)
         {
             Regex regex = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
             return regex.IsMatch(arg);
-        }
+        }*/
     }
 }
