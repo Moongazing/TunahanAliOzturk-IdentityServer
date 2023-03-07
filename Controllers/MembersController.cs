7013e3d9dca526992c4c321968d9e19705c86c50
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-03-08 01:36:44 +0300

    Adding Logout

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TAO.IdentityApp.Web.Models;
+
+namespace TAO.IdentityApp.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        public MembersController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
